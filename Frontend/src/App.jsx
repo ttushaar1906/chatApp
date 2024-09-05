1276ba2fb176abd9eab6d3fcf9fc949dfe87fb7d
Tushar Tharwani <ttushaar45@gmail.com>
2024-09-06 01:05:04 +0530

    Icons added and some changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
+import { FaVideo } from "react-icons/fa";
+import { IoCall } from "react-icons/io5";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { FaArrowLeft } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
 
 let socket;
 const CONNECTION_PORT = "http://localhost:3000"; // Ensure this matches your backend
@@ -56,6 +61,7 @@ function App() {
     console.log("Message after setMessage:", message);
   };
 
+  const logout = () => setLoggedIn(false);
   return (
     <>
       <div className=" bg-secondary">
@@ -92,7 +98,21 @@ function App() {
         ) : (
           <div className="p-2 bg-primary h-screen">
             <h1 className=" text-center p-4 font-bold text-2xl">Chatgram</h1>
-            <div className="relative flex flex-col justify-center w-3/5 mx-auto h-[80vh] bg-chatBubbleSent">
+            <div className="relative flex flex-col justify-center w-3/5 mx-auto h-[80vh] bg-chatBubbleSent border-2">
+              <div className="flex bg-secondary z-50 align-middle justify-between py-2 px-4 border-b-2">
+                <div className="flex gap-4 align-middle">
+                  <FaArrowLeft className="my-auto cursor-pointer text-sm" onClick={logout} />
+                  <FaUserCircle className="my-auto text-4xl"/>
+                  <h2 className="font-extrabold text-lg bg-secondary my-auto">
+                    {room}
+                  </h2>
+                </div>
+                <div className="flex gap-6 align-middle">
+                  <FaVideo className="my-auto cursor-pointer" />
+                  <IoCall className="my-auto cursor-pointer" />
+                  <BsThreeDotsVertical className="my-auto cursor-pointer" />
+                </div>
+              </div>
               <div className="absolute inset-0 overflow-hidden">
                 <img
                   src="../src/images/chatBg.avif"
@@ -101,7 +121,7 @@ function App() {
                 />
               </div>
               <div
-                className="border p-4 bg-gray-100 overflow-y-auto flex-1"
+                className=" p-4 bg-gray-100 overflow-y-auto flex-1"
                 style={{ position: "relative" }}
               >
                 {messageList.map((val, key) => {
@@ -142,7 +162,7 @@ function App() {
                   );
                 })}
               </div>
-              <div className="flex border z-50 ">
+              <div className="flex border-t-2 z-50 ">
                 <input
                   type="text"
                   placeholder="Type your message..."
@@ -153,7 +173,7 @@ function App() {
                   }}
                 />
                 <button
-                  className="bg-secondary py-2 px-4 font-semibold hover:opacity-90"
+                  className="bg-secondary py-2 px-4 font-bold hover:opacity-90"
                   onClick={sendMessage}
                 >
                   Send
